6d1baadb98fb7649e237c2faefd3bf009816f4cf
Robert Romano <robromano@macbookpro.mynetworksettings.com>
2024-11-22 10:50:12 -0500

    Add test data for menu and menu items.

--- src/Pages/Menu/MenuItem.tsx
+++ src/Pages/Menu/MenuItem.tsx
@@ -1,14 +1,23 @@
 import { FC, ReactElement } from "react";
 
+interface Item {
+  name: string;
+  description: string;
+}
+
 interface MenuItemsProps {
-  item: { name: string };
+  item: Item;
 }
 
 const MenuItem: FC<MenuItemsProps> = ({ item }): ReactElement => {
-  const { name } = item;
+  const { name, description } = item;
   return (
     <>
-      <div>{name}</div>
+      <div className="shadow-md rounded-lg hover:cursor-pointer hover:shadow-2xl">
+        <div className="px-4 pt-4 font-bold">{name}</div>
+        <div className="px-4 pb-4 text-sm text-gray-500">{description}</div>
+        <div className="pt-8 pl-4 pb-4 text-sm">$9.95</div>
+      </div>
     </>
   );
 };
--- src/Pages/Menu/MenuItems.tsx
+++ src/Pages/Menu/MenuItems.tsx
@@ -6,12 +6,19 @@ import MenuItem from "./MenuItem";
 //   items: [];
 // }
 
-const items = [{ name: "Cheese Pizza" }, { name: "Pepperoni Pizza" }];
+const items = [
+  { name: "Cheese Pizza", description: "This is a short description" },
+  { name: "Pepperoni Pizza", description: "This is a short description" },
+  { name: "Chicken Pizza", description: "This is a short description" },
+  { name: "Sausage Pizza", description: "This is a short description" },
+  { name: "Pineapple Pizza", description: "This is a short description" },
+  { name: "Grandma Pizza", description: "This is a short description" },
+];
 
 const MenuItems = (): ReactElement => {
   return (
     <>
-      <div className="grid grid-cols-2 mt-12">
+      <div className="grid grid-cols-2 mt-12 gap-6">
         {items.map((item) => (
           <MenuItem item={item} />
         ))}
